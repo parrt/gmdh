e4f93db4d95da3d62c0af3b95e65f04716f1cfd0
Terence Parr <parrt@cs.usfca.edu>
2016-09-06 14:53:38 -0700

    fix bug

--- SGD.py
+++ SGD.py
@@ -45,24 +45,25 @@ cost = 1e20
 while True:
     steps += 1
     prevcost = cost
-    # what is cost at current location?
-    # compute finite difference for one parameter
-    # (f(pos+h) - f(pos-h)) / 2h
-    dir = random.randint(0,num_parameters-1) # randint() is inclusive on both ends
-    pos.add_to_parameter(dir, h)
 
+    # what is cost at current location?
     MINIBATCH = 30
     indexes = np.random.randint(0,len(X),size=MINIBATCH)
     samples = X[indexes]
     sample_labels = labels[indexes]
 
+    # compute finite difference for one parameter
+    # (f(pos+h) - f(pos-h)) / 2h
+    dir = random.randint(0,num_parameters-1) # randint() is inclusive on both ends
     save = pos.get_parameter(dir)
     pos.add_to_parameter(dir, h)
     right = pos.cost(samples, sample_labels)
     pos.set_parameter(dir, save)
+
     pos.add_to_parameter(dir, -h)
     left = pos.cost(samples, sample_labels)
     pos.set_parameter(dir, save) # restore position vector
+
     finite_diff = (right - left) / (2*h)
     # move position in one direction only
     pos.add_to_parameter(dir, -eta * finite_diff) # decelerates x jump as it flattens out
